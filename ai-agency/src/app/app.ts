1e69b5e102ba3a7944445d28cd66b5df6906b02c
NicoEntra <nickstark91@icloud.com>
2026-04-26 17:42:08 -0500

    update

--- ai-agency/src/app/app.ts
+++ ai-agency/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('agency-ai');
+  protected readonly title = signal('ai-agency');
 }
